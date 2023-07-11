117bccf48d00c7c3c0d8f512610738795a28472a
Ramson <ramson.odoi@amalitech.org>
2023-07-11 12:03:36 +0000

    addidtion of another component

---
+++ src/app/tertiary-button/tertiary-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tertiary-button',
+  templateUrl: './tertiary-button.component.html',
+  styleUrls: ['./tertiary-button.component.css']
+})
+export class TertiaryButtonComponent {
+
+}
